37ffab6b43831743fe165073237437786b402c84
Jose Castillo <exmo800@gmail.com>
2019-04-18 23:28:26 -0400

    Project 01 puzzle
    Porject 01 Maze"void"

---
+++ PROJECT 01/Assets/Scripts/BlueCollect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BlueCollect : MonoBehaviour
+{
+    public Text scoretext;
+    public GameObject BLUEDOOR;
+    int score = 0;
+
+    private void Start()
+    {
+        scoretext.text = "Blue = " + score.ToString();
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        if (otherObject.gameObject.tag == "Coin2")
+        {
+            score++;
+
+
+
+            if (score == 5)
+            {
+                scoretext.text = "Blue = 5/" + score.ToString();
+                Destroy(BLUEDOOR);
+            }
+            else
+            {
+                scoretext.text = "Blue = " + score.ToString();
+            }
+
+            Destroy(otherObject.gameObject);
+            Debug.Log(score);
+        }
+    }
+
+}
---
+++ PROJECT 01/Assets/Scripts/Collect.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Collect : MonoBehaviour
+{
+    public Text scoretext;
+    public GameObject RedDoor;
+    int score = 0;
+
+    private void Start()
+    {
+        scoretext.text = "Red = " + score.ToString();
+        
+    }
+
+    private void OnCollisionEnter2D(Collision2D otherObject)
+    {
+        if (otherObject.gameObject.tag == "coin")
+        {
+            score++;
+
+           
+
+            if (score == 5)
+            {
+              scoretext.text = "Red = 5/" + score.ToString();
+                Destroy(RedDoor);
+            }
+            else
+            {
+                scoretext.text = "Red = " + score.ToString();
+            }
+
+            Destroy(otherObject.gameObject);
+            Debug.Log(score);
+        }
+    }
+
+}
